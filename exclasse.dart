e5784595200ef641ec9b6279b3c4d7b8fba2f1a3
Marcolima1765 <125322405+Marcolima1765@users.noreply.github.com>
2023-09-13 20:46:40 -0300

    Criação de classes

--- exclasse.dart
+++ exclasse.dart
@@ -1,11 +1,33 @@
 // criar classe chamada casa
 class Casa {
   String? cor; // atributo cor
+
+
+// criar metodos
+void abrirJanela(int qtde_janelas){
+  print("Abrir janela, qtde de janelas $qtde_janelas");
 }
 
+void abrirPorta(){
+  print("Porta aberta");
+}
+
+void abrirCasa(){
+  this.abrirJanela(2);
+  this.abrirPorta();
+}
+}
 void main() {
   // Instancia a classe com o objeto
   Casa minhaCasa = Casa();
   minhaCasa.cor = "Blue";
   print(minhaCasa.cor);
+  minhaCasa.abrirPorta();
+  minhaCasa.abrirJanela(3);
+  minhaCasa.abrirCasa();
+  Casa imovel1 = Casa();
+  imovel1.cor = "Vermelho";
+  print(imovel1.cor);
+  imovel1.valor = 550.000;
+  print("O valor do imovel 1 é R\${imovel1.valor} ");
 }
