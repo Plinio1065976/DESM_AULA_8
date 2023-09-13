cb3836f6ba962a1461d1a0c3b6fe42ea404174d0
Marcolima1765 <125322405+Marcolima1765@users.noreply.github.com>
2023-09-13 20:02:44 -0300

    Cria função principal do programa

--- exclasse.dart
+++ exclasse.dart
@@ -1,4 +1,11 @@
 // criar classe chamada casa
-class casa {
+class Casa {
   String? cor; // atributo cor
+}
+
+void main() {
+  // Instancia a classe com o objeto
+  Casa minhaCasa = Casa();
+  minhaCasa.cor = "Blue";
+  print(minhaCasa.cor);
 }
